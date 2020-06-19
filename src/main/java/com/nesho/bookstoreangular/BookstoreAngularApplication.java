bd68c399741c2ba9aea84efc8d425b99fe3aa38d
gowthamp24 <gowthamp245@gmail.com>
2020-06-19 13:17:01 +0530

    Comments Added

--- src/main/java/com/nesho/bookstoreangular/BookstoreAngularApplication.java
+++ src/main/java/com/nesho/bookstoreangular/BookstoreAngularApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookstoreAngularApplication implements CommandLineRunner {
-
+//autowiring
     @Autowired
     private UserServiceImpl userService;
 
